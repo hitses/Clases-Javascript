1434ce2504273234039dc8a1c545a77ec6e07890
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2022-02-16 21:18:20 +0100

    Añadidos ejercicios de variables

---
+++ 02-tiposVariables/Ejercicios/ejercicios.js
@@ -0,0 +1,57 @@
+/* 
+  Definir las siguientes variables con el nombre que creas más apropiado:
+  String -> Hola
+  String -> Carlos
+  Number -> 29
+  String -> Calle Falsa
+  Number -> 2022
+  Array  -> [VariableCalleFalsa, 123] // Añadir al Array la variable String 'Calle Falsa'
+ */
+
+/* 
+  Mostrar por consola la siguiente información por separado (la ortografía es importante en este trabajo):
+  - El año debes obtenerlo restando el año actual junto a tu edad.
+  'Hola, me llamo Carlos. Tengo 29 años y vivo en la Calle Falsa, 123. Nací en el año 1992.'
+ */
+
+/* 
+  Crea un objeto con la siguiente estructura:
+  person = {
+    address: {
+      name: string,
+      number: number
+    },
+    age: number,
+    familiars: [
+      father: {
+        name: string,
+        age: number
+      },
+      mother: {
+        name: string,
+        age: number
+      },
+      brother: {
+        name: string,
+        age: number
+      },
+    ],
+    name: string,
+  }
+ */
+
+/* 
+  Mostrar por consola la siguiente información por separado (la ortografía es importante en este trabajo):
+  - La diferencia de edad entre tu hermano y tú es restándolas.
+  'Mis padres, {padre} y {madre}, y tiene {edad del padre} y {edad de la madre}. Viven en la {dirección}. En casa también vive mi {hermano}, de {edad del hermano}. Eso quiere decir que nos llevamos {diferencia de edad entre tú y tu hermano}.'
+ */
+
+/* 
+  Declara una función que imprima:
+  'Hola mundo.'
+ */
+
+/* 
+  Declara una función que, pasándole una lista con la estructura [nombre, edad, dirección] o la estructura [nombre, edad, municipio, dirección] imprima por pantalla:
+  'Hola {nombre}. Veo que tienes {edad} y vives en {dirección}.'
+ */
