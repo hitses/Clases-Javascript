4733e28b174adde9edce48580168ed45da18c7dd
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2022-02-24 16:50:55 +0100

    Ejercicios de control de flujo agregados

--- 02-tiposVariables/Ejercicios/02ejercicios.js
+++ 02-tiposVariables/Ejercicios/02ejercicios.js
@@ -1,7 +1,32 @@
 // Crear una constante numérica e imprimir por pantalla si el número es par o impar
 
-const numero1 = 23
+const num = 23
 
 const parImpar = num => num % 2 === 0 ? 'Es par' : 'Es impar'
 
-console.log(parImpar(numero1))
+console.log(parImpar(num))
+
+// Crear dos constantes 'diámetro' y 'grosor' de una rueda (en metros) y, a través de cualquier tipo de declaración de control de flujo siempre teniendo en cuenta la mejor legibilidad y la menor cantidad de espacio utilizado, realizar las siguientes operaciones:
+// a) Si el diámetro es superior a 1.4m debe mostrar el mensaje “La rueda es para un vehículo grande”. Si es menor o igual a 1.4m pero mayor que 0.8m debe mostrar el mensaje “La rueda es para un vehículo mediano”. Si no se cumplen ninguna de las condiciones anteriores debe mostrar por pantalla el mensaje “La rueda es para un vehículo pequeño”.
+// b) Si el diámetro es superior a 1.4m con un grosor inferior a 0.4m o si el diámetro es menor o igual a 1.4m pero mayor que 0.8m con un grosor inferior a 0.25m, deberá mostrar el mensaje “El grosor para esta rueda es inferior al recomendado”
+
+/* Escribir un script que responda a un usuario que quiere comprar un helado cuánto le costará en función del topping que elija.
+El helado sin topping cuesta 1.90€.
+El topping de Oreo cuesta 1€.
+El topping de KitKat cuesta 1.50€.
+El topping de brownie cuesta 0.75€.
+El topping de Lacasitos cuesta 0.95€.
+En caso de no disponer del topping solicitado por el usuario el programa escribirá por pantalla 'No tenemos este topping, lo sentimos. ' y a continuación informar del precio del helado sin ningún topping.
+Finalmente, el programa escribe por pantalla el precio del helado con el topping seleccionado (o ninguno).
+
+Se puede utilizar cualquier tipo de declaración de control de flujo siempre teniendo en cuenta la mejor legibilidad y la menor cantidad de espacio utilizado.*/
+
+// Realizar un script que indique a un alumno su nivel académico dependiendo de su nota.
+// Si ha sacado de 0 a 4: suspenso.
+// Si ha sacado 5: aprobado.
+// Si ha sacado 6: bien.
+// Si ha sacado 7: notable.
+// Si ha sacado 8: excelente.
+// Si ha sacado 9: sobresaliente.
+// Si ha sacado 10: matrícula de honor.
+// Se puede utilizar cualquier tipo de declaración de control de flujo siempre teniendo en cuenta la mejor legibilidad y la menor cantidad de espacio utilizado.
